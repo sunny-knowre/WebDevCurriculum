f661fe56978eb697a0cc6233c45b42acd4bfd957
Sunny Chung <skchung@knowre.com>
2017-11-16 21:14:01 +0900

    Part 2 - Quest 6 added some testing css and outline for js

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -1,15 +1,76 @@
-var Desktop = function() {
+var Desktop = function(dom, iconCount, folderCount) {			
+	this.dom = dom;
+	var dragging = null;	// keep track of dragging object
+	this.windowList = [];   // keep list of folder windows hidden
+
+	function addItem(item,name){
+		// add item to desktop	
+	}
+
+	function initialize(iconCount,folderCount){
+		for(var i=0; i<iconCount; i++){
+			addItem("icon","icon-"+i);
+		}
+		for(var i=0; i<folderCount; i++){
+			addItem("folder","folder-"+i);
+		}
+		// use counts to set up desktop
+	};
+		
+	// event handler
+	function handleEvent(event){
+		var target = event.target;
+		switch(event.type){
+			case "mousedown":
+				// handle mouse down event set dragging to target
+				break;
+
+			case "mousemove":
+				if (dragging !== null){
+					//assign location
+				} 
+				break;
+
+			case "mouseup":
+				dragging = null;	// reset dragging
+				break;
+
+			case "dblclick":
+				// fire opening window event
+			
+			break;
+		}
+	}
+	// attach listeners
+	this.dom.addEventListener("dblclick",handleEvent,false);
+	this.dom.addEventListener("mousedown",handleEvent,false);
+	this.dom.addEventListener("mouseup",handleEvent,false);
+	this.dom.addEventListener("mousemove",handleEvent,false);
+
+	initialize(iconCount,folderCount);
 	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 };
 
-var Icon = function() {
+var Icon = function(name) {
+	var newDom = // make html element
+	this.name = name ? name : "";
+	this.dom = newDom;
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 };
 
-var Folder = function() {
+var Folder = function(name) {
+	var newDom = // make html element
+	this.name = name ? name : "";
+	this.dom = newDom;		
 	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 };
 
-var Window = function() {
+var Window = function(name) {
+	var newDom = // make html element
+	this.name = name ? name : "";
+	this.dom = newDom;
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 };
+Window.prototype.showWindow(){
+	// do stuff to show window
+}
