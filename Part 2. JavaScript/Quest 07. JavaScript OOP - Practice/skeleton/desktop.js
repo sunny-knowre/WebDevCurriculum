65c24fe1c3dd1e0b3ee26b4d6952b33c1a1ffd8d
Sunny Chung <skchung@knowre.com>
2017-11-22 17:14:26 +0900

    Part 2 - Quest 7 applied refactoring item 1

--- Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
@@ -1,140 +1,111 @@
 /* feedback notes:
-	1. decouple by moving logic to Icon & Folder classes
+	* 1. decouple by moving logic to Icon & Folder classes
 	2. use event bubbling to remove desktop context from constructors
 		- research stopPropagation() and preventDefault() on event
 		- how to fix even not firing when mouse events are on individual icons
 */
-var Desktop = function(dom, iconCount, folderCount) {
+var Desktop = function (dom, iconCount, folderCount) {
 	var uid = 0;
 	var dragging = null;
-	var zIndex = 100;
-	var itemList = [];
-	var diffX = 0;
-	var diffY = 0;
+	var self = this;
 	this.dom = dom;
-	
-	function addItem(item){
-		if(item === "icon"){
-			var name = "icon" + uid++;
-			var newIcon = new Icon(name,dom);
-			itemList[name] = newIcon;
-		} else if (item === "folder"){
-			var name = "folder" + uid++;
-			var winName = "window-" + name;
-			var newFolder = new Folder(name,dom);
-			itemList[name] = newFolder;
-			itemList[winName] = newFolder.window;
-		}
-		// add item to desktop	
-	}	
 
-	function initialize(iconCount,folderCount){
-		for(var i=0; i<iconCount; i++){
+	(function initialize(iconCount, folderCount) {
+		for (var i = 0; i < iconCount; i++) {
 			addItem("icon");
 		}
-		for(var i=0; i<folderCount; i++){
+		for (var i = 0; i < folderCount; i++) {
 			addItem("folder");
 		}
-		// use counts to set up desktop
-	};
-		
-	// event handler
-	
-	function handleEvent(event){
-		var target = event.target;
-		var item = itemList[target.id];
-		
-		switch(event.type){
-			case "mousedown":
-				if( item && target.className.indexOf("draggable") > -1 ){
-					dragging = item;
-					item.dom.style.zIndex = zIndex++;
-					diffX = (event.pageX + item.margin) - item.dom.offsetLeft;
-					diffY = (event.pageY + item.margin) - item.dom.offsetTop;
-				}
-				break;
+	}(iconCount,folderCount));
 
-			case "mousemove":
-				if (dragging !== null){
-					//assign location
-					dragging.move((event.pageX - diffX), (event.pageY - diffY));
-				} 
-				break;
-
-			case "mouseup":
-				dragging = null;
-				break;
-
-			case "dblclick":
-				if(!(item.window === "undefined")){
-					item.window.dom.style.display = "block";	
-					item.window.titleBar.classList.add("draggable");
-					item.window.dom.style.zIndex = zIndex++;
-				}
-			break;
+	function addItem(item) {
+		if (item === "icon") {
+			var name = "icon" + uid++;
+			var newItem = new Icon(name);
+		} else if (item === "folder") {
+			var name = "folder" + uid++;
+			var newItem = new Folder(name);
 		}
+		newItem.dom.addEventListener('mousedown', function(e){
+			dragging = newItem;
+		});
+		self.dom.appendChild(newItem.dom);
+		// add item to desktop	
 	}
-	// attach listeners
-	document.addEventListener("dblclick",handleEvent,false);
-	document.addEventListener("mousedown",handleEvent,false);
-	document.addEventListener("mouseup",handleEvent,false);
-	document.addEventListener("mousemove",handleEvent,false);
-	initialize(iconCount,folderCount);
+	
+	document.addEventListener('mouseup', function(e){
+		dragging = null;
+	});
+	document.addEventListener('mousemove', function(e){
+		if(dragging){
+			dragging.move(e.pageX, e.pageY);
+		}
+	});
 	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 };
 
-var Icon = function(name, desktop) {
+var Icon = function (name) {
+	this.diffX = 0;
+	this.diffY = 0;
 	this.type = "icon";
 	this.name = name;
-	this.desktop = desktop;
 	this.dom = this.makeDom();
-	this.dom.setAttribute("id",this.name);
-	this.dom.innerHTML = this.name;
-	this.dom.classList.add("draggable");
-
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 };
 Icon.prototype = {
-	"margin" : 10,
-	"makeDom" : function(){
-		var newDiv = document.createElement('div'); 
-		newDiv.style.margin = this.margin + "px";
-		newDiv.classList.add(this.type);
-		this.desktop.appendChild(newDiv);
-		return newDiv;
+	"zIndex"  : 100,
+	"margin"  : 10,
+	"makeDom" : function (){
+					var newDiv = document.createElement('div');
+					newDiv.style.margin = this.margin + "px";
+					newDiv.classList.add(this.type);
+					newDiv.setAttribute("id", this.name);
+					newDiv.innerHTML = this.name;
+					newDiv.classList.add("draggable");
+					newDiv.addEventListener("mousedown", this, false);
+					return newDiv;
+				},
+	"move" : function (x, y) {
+			var offsetX = x - this.diffX;
+			var offsetY = y - this.diffY;
+			this.dom.style.position = "absolute";
+			this.dom.style.left = offsetX + "px";
+			this.dom.style.top = offsetY + "px";
 	},
-	"move" : function(x,y){
-		this.dom.style.position = "absolute";
-		this.dom.style.left = x + "px";
-		this.dom.style.top = y + "px";
+	"handleEvent": function (e) {
+		switch(e.type){
+			case 'mousedown':
+				this.dom.style.zIndex = Icon.prototype.zIndex++;
+				this.diffX = (e.pageX + this.margin) - this.dom.offsetLeft;
+				this.diffY = (e.pageY + this.margin) - this.dom.offsetTop;
+				break;
+		}
 	}
-}
+};
 
-var Folder = function(name,desktop) {
+var Folder = function (name) {
 	this.type = "folder";
 	this.name = name;
-	this.desktop = desktop;
 	this.dom = this.makeDom();
-	this.dom.setAttribute("id",this.name);
+	this.dom.setAttribute("id", this.name);
 	this.dom.classList.add("draggable");
 	this.dom.innerHTML = this.name;
-	this.window = new Window(name,desktop);
 	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 };
 Folder.prototype = Object.create(Icon.prototype);
 Folder.prototype.constructor = Folder;
 
-var Window = function(name,desktop) {
+var Window = function (name) {
 	this.type = "window";
 	this.name = name;
-	this.desktop = desktop;
 	this.margin = 0;
 	this.dom = this.makeDom();
-	
-	var titleDiv = document.createElement('div');	
+
+	var titleDiv = document.createElement('div');
 	titleDiv.classList.add("title-bar");
 	titleDiv.innerHTML = this.name;
-	titleDiv.setAttribute("id", "window-" + this.name);		
+	titleDiv.setAttribute("id", "window-" + this.name);
 	this.dom.appendChild(titleDiv);
 	this.titleBar = titleDiv;
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
