f196209e815ec8e83ea6699d3b8e3ba28541c079
Sunny Chung <skchung@knowre.com>
2018-01-24 09:50:18 +0900

    Part 6: Final - configure webpack for async await

--- Part 6. Final Project/Quest 20. Final Project/submit/client/src/components/Home.vue
+++ Part 6. Final Project/Quest 20. Final Project/submit/client/src/components/Home.vue
@@ -1,3 +1,33 @@
 <template>
-  <p></p>
+  <div><button @click="sendData">send</button></div>
 </template>
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      activityData: [
+        { id: 1, name: '4x4 boulder', metric: 'V grade', description: 'only 1 min rest between sets', type: 4 },
+        { id: 2, name: 'ARC', metric: 'mins', description: 'climb continuously for x minutes', type: 1 },
+        { id: 3, name: 'x-on x-off', metric: 'mins', description: 'climb for x min and rest x min; 4 reps', type: 1 },
+        { id: 4, name: '4x4 route', metric: 'decimal grade', description: '3 or more set of 4 routes', type: 1 },
+        { id: 5, name: 'one/min', metric: 'grade', description: 'climb 1 boulder/min for 8 min; 3 reps', type: 3 },
+        { id: 6, name: 'deadhang', metric: '', description: '', type: 1 },
+        { id: 7, name: 'plank', metric: 'mins', description: '', type: 6 },
+        { id: 8, name: 'pull up', metric: 'reps', description: '', type: 6 }
+      ]
+    }
+  },
+  methods: {
+    async sendData () {
+      try {
+        let result = await axios.put('https://vuejs-http-685b2.firebaseio.com/activities.json', this.activityData)
+        console.log(result)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
